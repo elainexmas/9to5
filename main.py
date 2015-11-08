1a3254f8c83d3adaeeae52bec472c780cf9acfb8
Elaine K. Lee <elee1225@gmail.com>
2015-11-07 23:21:19 -0600

    Argument parser; modify copy of commit

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+import argparse
 import time
 import git as gp
 from random import randint
 
 def _modify_one_commit(commit):
-    current_hour = time.localtime(commit.committed_date).tm_hour
+    copy = commit
+    current_hour = time.localtime(copy.committed_date).tm_hour
     if current_hour > 17 or current_hour < 9:
         # pick random hour in 9am-5pm
         random_hour = randint(9, 17)
         diff = current_hour - random_hour
-        commit.committed_date - diff*3600
+        copy.committed_date -= diff*3600
 
-    assert time.localtime(commit.committed_date).tm_hour >= 9 and time.localtime(commit.committed_date).tm_hour <= 17
-
-    return commit
+    assert time.localtime(copy.committed_date).tm_hour >= 9 and time.localtime(copy.committed_date).tm_hour <= 17
+    return copy
 
 def fix_times(path, branch = "master"):
-    # cd into the path of the repo
-    repo = gp.repo(path)
+
+    # create Repo object using path
+    repo = gp.Repo(path)
 
     # grab last commit
     last_commit = repo.heads[branch].commit
 
     # make changes
     repo.heads[branch].commit = _modify_one_commit(last_commit)
+
+def main():
+    parser = argparse.ArgumentParser("Modify git commit timestamps")
+    parser.add_argument('path', type = str, help = 'path to repo')
+    parser.add_argument('--branch', type = str, help = 'branch name', default = 'master')
+    args = parser.parse_args()
+    fix_times(args.path, args.branch)
+
+if __name__ == "__main__":
+    main()
 
